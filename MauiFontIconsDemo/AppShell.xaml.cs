9724aa3e86486be340b307a5c285001ef83cbb68
Cedric Gabrang <cedric.gabrang@hotmail.com>
2022-09-23 15:49:33 +0800

    Add project files.

---
+++ MauiFontIconsDemo/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiFontIconsDemo;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MauiFontIconsDemo/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MauiFontIconsDemo;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
